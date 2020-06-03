be3dfbb8bcfcd42855d2f56d817f17167b37e832
Travis Veazey <travisvz@gmail.com>
2020-06-03 10:49:48 -0800

    Move Header struct into header sub-module
    New sub-module is private, but we re-export Header from it so it's still
    useable by client code.

--- src/message.rs
+++ src/message.rs
@@ -1,33 +1,3 @@
-use crate::utils::bytes_to_u16;
+mod header;
 
-#[derive(Debug)]
-pub struct Header {
-    id: u16,
-    flags: u16,
-    qry_count: u16,
-    ans_count: u16,
-    auth_count: u16,
-    addl_count: u16,
-}
-
-impl From<&[u8]> for Header {
-    fn from(bytes: &[u8]) -> Header {
-        assert!(bytes.len() >= 12);
-
-        let id = bytes_to_u16(&bytes);
-        let flags = bytes_to_u16(&bytes[2..]);
-        let qry_count = bytes_to_u16(&bytes[4..]);
-        let ans_count = bytes_to_u16(&bytes[6..]);
-        let auth_count = bytes_to_u16(&bytes[8..]);
-        let addl_count = bytes_to_u16(&bytes[10..]);
-
-        Header {
-            id,
-            flags,
-            qry_count,
-            ans_count,
-            auth_count,
-            addl_count,
-        }
-    }
-}
+pub use header::Header;
