28519657776e9b70e01bcb37bb9143483b3db27e
Travis Veazey <travisvz@gmail.com>
2020-06-09 10:19:20 -0800

    Stubbing RDATA decoding in Resource Records
    When printing a Resource Record, we now display a decoded A record (i.e.
    we show the target IP address) or, for other records, still show the
    bytes (in hex format).

--- src/message/record.rs
+++ src/message/record.rs
@@ -103,19 +103,28 @@ impl ResourceRecord {
 
         bytes
     }
+
+    fn format_rdata(&self) -> String {
+        match self.rrtype {
+            RRType::A => format!("{}.{}.{}.{}", self.data[0], self.data[1], self.data[2], self.data[3]),
+            _ => {
+                let mut bytes = format!("");
+                for byte in self.data.iter() {
+                    bytes = format!("{}{:02X} ", bytes, byte);
+                }
+
+                bytes
+            }
+        }
+    }
 }
 
 impl fmt::Display for ResourceRecord {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut bytes = format!("");
-        for byte in self.data.iter() {
-            bytes = format!("{}{:02X} ", bytes, byte);
-        }
-
         write!(
             f,
             "{} {} {:?} {:?} {}",
-            self.label, self.ttl, self.class, self.rrtype, bytes
+            self.label, self.ttl, self.class, self.rrtype, self.format_rdata()
         )
     }
 }
